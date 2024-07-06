27a842ff4d55b4a5587d1d22bd738dd3e34e07f6
Don Aime <118371312+DUSHIME1212@users.noreply.github.com>
2024-07-06 22:14:34 +0200

    fix: task 2

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,21 +1,15 @@
 #!/usr/bin/python3
-"""
-function that queries the 'Reddit API'
-"""
+""" 0-subs.py """
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    number of subscribers
-    """
+    """ number_of_subscribers """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}  # avoid Too Many Requests error
-
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
-        data = response.json()
-        return data['data']['subscribers']
+        return response.json()["data"]["subscribers"]
     else:
         return 0
